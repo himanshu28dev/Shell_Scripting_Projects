dc6149037611403f3637d6d37f986430db8b7489
himanshu28dev <singhg2822@gmail.com>
2026-01-08 19:17:08 +0530

    Update and rename deploy-notes-app to djnago-notes-app/deploy-notes-app.sh

---
+++ djnago-notes-app/deploy-notes-app.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+#
+
+<< task
+Deploy a Django app
+And Handle the code for erros
+task
+
+code_clone() {
+        echo "Cloning the Django notebook application..."
+        git clone https://github.com/LondheShubham153/django-notes-app.git
+
+}
+
+install_requirements() {
+        echo "Installing the requirements......"
+        sudo apt install docker.io nginx -y
+}
+
+required_restart() {
+        sudo chown $USER /var/run/docker.sock
+        sudo systemctl enable docker
+        sudo systemctl enable nginx
+        sudo systemctl restart docker
+}
+
+deploy() {
+        docker build -t notes-app .
+        docker run -d -p 8000:8000 notes-app:latest
+}
+
+echo "***********Deployment Started************"
+
+if ! code_clone; then
+        echo "Clone directory already exists"
+        cd django-notes-app
+fi
+
+if ! install_requirements; then
+        echo "Installation failed"
+        exit 1
+fi
+
+if ! required_restart; then
+        echo "system requirement failed"
+        exit 1
+fi
+
+deploy
+
+echo "************Deployment Ended*************"
